d50167398479c87ffc00144b55c02d0950c9168f
Hiagor Vieira <hiagorhenrique2015@gmail.com>
2021-11-10 15:03:47 -0300

    Criação da ProdutoModel

--- Controller/ProdutoController/CadastroProdutoController.cs
+++ Controller/ProdutoController/CadastroProdutoController.cs
@@ -11,5 +11,6 @@ namespace CrudAugustusFashion.Controller.ProdutoController
             frmCadastroProduto.MdiParent = MdiSingletonModel.InstanciarMDI();
             frmCadastroProduto.Show();
         }
+
     }
 }
---
+++ Model/Produto/ProdutoModel.cs
@@ -0,0 +1,21 @@
+﻿namespace CrudAugustusFashion.Model.Produto
+{
+    public class ProdutoModel
+    {
+        public int IdProduto { get; set; }
+        public string Nome{ get; set; }
+        public decimal PrecoVenda{ get; set; }
+        public decimal PrecoCusto{ get; set; }
+        public string CodigodeBarras{ get; set; }
+        public string Fabricante { get; set; }
+        public int QuantidadeEstoque { get; set; }
+        public Status StatusProduto { get; set; }
+
+
+        public enum Status
+        {
+            ativo = 1,
+            inativo = 0
+        }
+    }
+}
