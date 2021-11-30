a8d86b9c80cc6cd677cad86c7ce49a0f7249f6ad
Hiagor Vieira <hiagorhenrique2015@gmail.com>
2021-11-30 18:07:53 -0300

    Idade adicionada

--- Model/ClienteModel/ClienteListaModel.cs
+++ Model/ClienteModel/ClienteListaModel.cs
@@ -8,7 +8,11 @@ namespace CrudAugustusFashion.Model
         public int IdCliente { get; set; }
         public NomeCompleto NomeCompleto { get; set; }
         public string Sexo { get; set; }
-        public int Idade { get; set; }
+        public int Idade { get =>
+            DataNascimento.DayOfYear >= DateTime.Now.DayOfYear ?
+                    DateTime.Now.Year - DataNascimento.Year : DateTime.Now.Year - DataNascimento.Year - 1;
+        }
+
         public DateTime DataNascimento { get; set; }
         public EnderecoModel Endereco { get; set; }
         public TelefoneModel Telefone { get; set; }
--- Model/ColaboradorModel/ColaboradorListaModel.cs
+++ Model/ColaboradorModel/ColaboradorListaModel.cs
@@ -8,7 +8,16 @@ namespace CrudAugustusFashion.Model
         public int IdColaborador { get; set; }
         public NomeCompleto NomeCompleto { get; set; }
         public string Sexo { get; set; }
-        public int Idade { get; set; }
+        public int Idade { get
+            {
+                if(DataNascimento.DayOfYear <= DateTime.Now.DayOfYear)
+                    return DateTime.Now.Year - DataNascimento.Year;
+                
+                    return DateTime.Now.Year - DataNascimento.Year -1;
+
+            } 
+            //DataNascimento.Year - DateTime.Now.Year;
+        }
         public DateTime DataNascimento { get; set; }
         public EnderecoModel Endereco { get; set; }
         public TelefoneModel Telefone { get; set; }
